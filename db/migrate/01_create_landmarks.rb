7660782b991b6a23b7a1483ef6f3c698498d5a19
github username <github email address>
2017-01-19 15:51:54 -0500

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,6 +1,6 @@
 class CreateLandmarks < ActiveRecord::Migration
   def change
-    create_table :artists do |t|
+    create_table :landmarks do |t|
       t.string :name
       t.integer :figure_id
       t.integer :year_completed
--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
-  create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "figure_id"
-    t.integer  "year_completed"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
   create_table "figure_titles", force: :cascade do |t|
     t.integer  "title_id"
     t.integer  "figure_id"
@@ -32,6 +24,14 @@ ActiveRecord::Schema.define(version: 4) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "landmarks", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "figure_id"
+    t.integer  "year_completed"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "titles", force: :cascade do |t|
