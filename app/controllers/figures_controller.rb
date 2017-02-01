b9b50df7db7ec0e7b789529da23f8f77399baaf4
github username <github email address>
2017-02-01 14:10:51 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,13 +1,17 @@
 require 'pry'
 class FiguresController<ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
   get '/figures/new' do
     erb :"figures/new"
   end
 
   post '/figures' do
     @figure = Figure.create(name: params["figure_name"])
-
     if !params["new_title"].empty?
       @figure.titles << Title.create(name: params["new_title"])
     end
@@ -17,7 +21,6 @@ class FiguresController<ApplicationController
         @figure.titles << title
       end
     end
-
     if !params["new_landmark"].empty?
       @figure.landmarks << Landmark.create(name: params["new_landmark"])
     end
@@ -28,6 +31,11 @@ class FiguresController<ApplicationController
       end
     end
     @figure.save
+    redirect to "figures/#{@figure.id}"
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
 end
