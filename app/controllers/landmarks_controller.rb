5a09c8e868d8d7f435ba137a6ac3e818e18b2050
github username <github email address>
2017-02-01 19:33:52 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,12 +1,25 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+
   get '/landmarks/new' do
     erb :"landmarks/new"
   end
 
   post '/landmarks' do
-    binding.pry
-    @landmark = Landmark.create(name: ["new_landmark"])
+    @landmark = Landmark.create(name: ["new_landmark"], year_completed: ["year_completed"])
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
 end
